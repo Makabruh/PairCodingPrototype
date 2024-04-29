d931b460a1d2b0250f66891b0b244052a431b6bf
Emily <milly@roach.me.uk>
2024-04-29 12:13:38 +0100

    Added error boundaries for unforeseen errors - will need to be tested

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,18 +5,21 @@ import App from './App';
 import {AuthProvider} from './context/AuthProvider';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import ErrorBoundary from './errors/ErrorBoundary';
  
  
  
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-       <Routes>
-          <Route path="/*" element={<App />}></Route>
-        </Routes>
-      </AuthProvider>
-    </BrowserRouter>
+    <ErrorBoundary>
+      <BrowserRouter>
+        <AuthProvider>
+        <Routes>
+            <Route path="/*" element={<App />}></Route>
+          </Routes>
+        </AuthProvider>
+      </BrowserRouter>
+    </ErrorBoundary>
   </React.StrictMode>
 );
